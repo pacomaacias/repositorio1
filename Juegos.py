1351deb2761c252665a73af8fcf185d83fb1baee
pacomaacias <125871130+pacomaacias@users.noreply.github.com>
2023-03-15 13:46:31 +0100

    Create Juegos.py
    error

---
+++ Juegos.py
@@ -0,0 +1,21 @@
+class videojuegos:
+    def __init__(self,nnombre,nedad_aconsejable,nvideoconsola):
+        self.__nombren=nnombre
+        self.__edad_aconsejablen=nedad_aconsejable
+        self.__videoconsolan=nvideoconsola
+    def setNnombre(self,nnombre):
+        self.__nombren=nnombre
+    def setNnombre(self):
+        return self.__nombren
+    def setNedad_acosejable(self,nedad_aconsoable):
+        self.__edad_aconsejablen=nedad_aconsoable
+    def setNedad_aconsejable(self):
+        return self.__edad_aconsejablen
+    def setNvideoconsola(self,nvideoconsola):
+        self.__videoconsolan=nvideoconsola
+    def setNvideoconsola(self):
+        return self.__videoconsolan
+if __name__=="__main__":
+    class caracteristicas_videojuegos:
+        def __init__():
+
